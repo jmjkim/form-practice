429749ed09bed68332abb7d6a70d94fa0f81f0bd
Junsuk Park <park64kr63@gmail.com>
2023-02-14 16:12:06 +0900

    remove comment

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -20,24 +20,13 @@ const SimpleForm = ({children}: PropsWithChildren<{}>) => {
     const [errors, setErrors] = useState({});
     // const [valid, setValid] = useState(false);
     const value = useMemo(() => ({setValues, values, setErrors, errors}), [setValues, values, setErrors, errors]);
-    
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
-    
+
         const validInputs = Object.values(values).every(v => v !== "" && v.length > 0);
         const noErrors = Object.values(errors).every(err => err === undefined);
 
-        // const validateInputValues = (arr: any[]) => {
-        //     const valid = arr.every(value => value !== "" && value.length > 0);
-
-        //     if (valid) {
-        //         setValid(true);
-        //     }
-        // }
-
-        // validateInputValues(Object.values(values));
-
-        // if (valid && noErrors) {
         if (validInputs && noErrors) {
             alert(JSON.stringify(values));
         }
