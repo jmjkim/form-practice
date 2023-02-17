f63360ff1247c58b9feb1a46a0b628fac854e694
Junsuk Park <park64kr63@gmail.com>
2023-02-18 05:26:50 +0900

    fixing SelectboxField testing error

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -5,6 +5,7 @@
 import React from "react";
 import {fireEvent, render, screen} from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
+import "@testing-library/jest-dom";
 
 import {min, max, required} from "../src/utils/utils";
 import SimpleForm from '../src/components/SimpleForm';
@@ -12,55 +13,48 @@ import TextField from "../src/components/TextField";
 import SelectboxField from "../src/components/SelectboxField";
 import CheckboxField from "../src/components/CheckboxField";
 
-
-describe('Testing for <TextField>, <SelectboxField> and <CheckboxField>', () => {    
+describe('Testing for <TextField>, <SelectboxField> and <CheckboxField>', () => {
     it('#1 <TextField> - render error messages if validation (min(5), max(10)) fails', () => {
         render(
             <SimpleForm>
-                <TextField 
+                <TextField
                 source={'name'}
                 label={'이름'}
-                validate={[min(5), max(10)]}
-                />
-
-                <TextField 
-                source={'password'}
-                label={'비밀번호'}
                 validate={[min(5), max(10)]}
                 />
             </SimpleForm>
         )
 
         const name = screen.getByLabelText('이름');
-        const password = screen.getByLabelText('비밀번호');
 
         fireEvent.change(name, {target: {value: 'x'.repeat(4)}});
-        fireEvent.change(password, {target: {value: 'x'.repeat(4)}});
-        expect(screen.getAllByText(/이상 입력해주세요./))
-        
+        expect(screen.getByText(/이상 입력해주세요./))
+
         fireEvent.change(name, {target: {value: 'x'.repeat(11)}});
-        fireEvent.change(password, {target: {value: 'x'.repeat(11)}});
-        expect(screen.getAllByText(/이하로 입력해주세요./))
+        expect(screen.getByText(/이하로 입력해주세요./))
 
         // check valid input values
-        // fireEvent.change(name, {target: {value: 'x'.repeat(6)}});
-        // fireEvent.change(password, {target: {value: 'x'.repeat(6)}});
+        fireEvent.change(name, {target: {value: 'x'.repeat(6)}});
+
+        expect(screen.queryByText(/이하로 입력해주세요./)).toBeNull()
     })
-    
-    // it('#2 <SelectboxField> - render error messages if validation (required()) fails', () => {
-    //     render(
-    //         <SelectboxField
-    //         source={'gender'}
-    //         label={'성별'}
-    //         validate={[required()]}
-    //         />
-    //     );
-    //     const gender = screen.getByLabelText('성별') as HTMLInputElement;
-    //     userEvent.selectOptions(gender, '남');
-    //     userEvent.selectOptions(gender, '');
-    //     // screen.debug()
-    // })
-    
+
+    it('#2 <SelectboxField> - render error messages if validation (required()) fails', () => {
+        const {getByRole} = render(
+            <SimpleForm>
+                <SelectboxField
+                    source={'gender'}
+                    label={'성별'}
+                    validate={[required()]}
+                />
+            </SimpleForm>
+        );
+        const selectElement = getByRole('combobox');
+
+        fireEvent.change(selectElement, { target: { value: '남' } });
+        expect(selectElement).toHaveValue("남");
+    })
+
     // it('#3 <CheckboxField> - render error messages if validation (required()) fails', () => {
     //     render(
     //         <CheckboxField
